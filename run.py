e04cdda4f6e2426887eadacb2b610148379e7099
Vincent Bergeron <vincentbergeron33@gmail.com>
2023-03-03 15:13:16 +0000

    Adjust validation to check that all values provided are intergers

--- run.py
+++ run.py
@@ -36,6 +36,7 @@ def validate_data(values):
     """
 
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
